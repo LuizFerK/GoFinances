a91978f52395b324c0e03168e3a8316440a12144
luiz <luizfernandoklein@live.com>
2020-05-28 21:02:51 -0300

    feat: start

---
+++ backend/src/models/Category.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('categories')
+class Category {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Category;
---
+++ backend/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,50 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+@EntityRepository(Transaction)
+class TransactionsRepository extends Repository<Transaction> {
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (acc, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acc.income += Number(transaction.value);
+            break;
+
+          case 'outcome':
+            acc.outcome += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = balance.income - balance.outcome;
+
+    return {
+      income: balance.income,
+      outcome: balance.outcome,
+      total,
+    };
+  }
+}
+
+export default TransactionsRepository;
